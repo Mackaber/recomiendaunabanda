5fcd0637df859fc1e094db22deedbe08d8e634f3
Mackaber <x@mackaber.me>
2022-11-29 13:36:29 -0600

    fixed 2

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ function start(token,response) {
 
 	var addArtists = function(artists,res) {
 		res.artists.items.forEach(function(val) {
-		    artists.push({ "name": val.name, "followers": val.followers.total, "link": val.external_urls.spotify, "genres": val.genres, "image": val.images[0].url });
+		    artists.push(
+					{ name: val.name, 
+					  followers: val.followers.total, 
+						link: val.external_urls.spotify, 
+						genres: val.genres, 
+						image: val.images.length > 0 ? val.images[0].url : "https://appletoolbox.com/wp-content/uploads/2018/11/Blank-iTunes-Album-Cover-no-artwork.jpg"
+					});
 		});
 	}
 
@@ -23,14 +29,26 @@ function start(token,response) {
 	var displayResults = function(artists){
 		artists.sort(compare);
 
-		var artistSource = document.getElementById('artist-template').innerHTML,
-        	artistTemplate = Handlebars.compile(artistSource),
-        	artistPlaceHolder = document.getElementById('artist');
-        	artist = artists[0];
-        	name = response.display_name.length > 9 ? response.display_name.substr(0,8) : response.display_name
-        	genre = artist.genres.size > 0 ? artist.genres[0] : "na."
-        	artistPlaceHolder.innerHTML = artistTemplate({"user_name": name, "user_image": window.btoa(response.images[0].url), "artist": artist, "genre": genre, "image": window.btoa(artist.image)});
+		let artistSource = document.getElementById('artist-template').innerHTML
+    let artistTemplate = Handlebars.compile(artistSource)
+    let artistPlaceHolder = document.getElementById('artist');
 
+		let artist = artists[0];
+		let name = response.display_name.length > 9 ? response.display_name.substr(0,8) : response.display_name
+		let genre = artist.genres.size > 0 ? artist.genres[0] : "na."
+
+		let profile_image = response.images[0].url
+
+		let link = `https://res.cloudinary.com/recomiendaunabanda/image/upload/`;
+		link += `c_scale,l_fetch:${window.btoa(artist.image)},w_370,x_500,y_240/`;
+		link += `co_rgb:feef5e,e_colorize:100,l_text:Francois%20One_50_bold:${name},x_-320,y_-298/`;
+		link += `co_rgb:feef5e,e_colorize:100,l_text:Francois%20One_50_bold:${artist.followers},x_-50,y_-110/`;
+		link += `co_rgb:feef5e,e_colorize:100,l_text:Francois%20One_60_bold:${genre},x_120,y_55/`;
+		link += `co_rgb:feef5e,e_colorize:100,l_text:Francois%20One_50_bold:${artist.name},x_220,y_-298/`;
+		// link += `c_scale,l_fetch:${window.btoa(encodeURIComponent(response.images[0].url))},r_max,w_150,x_-550,y_-298/`
+		link += `c_crop/v1669749018/artist/final_recomienda.png`;
+
+		artistPlaceHolder.innerHTML = artistTemplate({ "link" : link });
 		$('#artist').show();
 		twttr.widgets.load(); 
 	}
